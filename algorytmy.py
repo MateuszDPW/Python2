6e294c43feb119c610169ebe33f4d89593305e1f
Mateo <01182344@pw.edu.pl>
2024-02-27 09:46:42 +0100

    kolejna lekcja

--- algorytmy.py
+++ algorytmy.py
@@ -40,10 +40,45 @@
 #wejscie: 1 liczba calkowita
 #wyjscie: lista/tablica
 
-def dzielnik(a):
-    div = []
-    for i in range(1, a+1):
-        if a % i ==0:
-            div.append(i)
-    print(div)
-dzielnik(8)
+# def dzielnik(a):
+#     div = []
+#     for i in range(1, a+1):
+#         if a % i ==0:
+#             div.append(i)
+#     print(div)
+# dzielnik(8)
+
+
+# # sortowanie przez wstawianie O(n^2)
+# import random
+# import time
+
+# def wstaw(tab):
+#     for i in range(1, len(tab)):
+#         siu = tab[i]
+#         j = i-1
+#         while j>=0 and siu < tab[j]:
+#             tab[j+1] = tab[j]
+#             j -= 1
+#         tab[j+1] = siu
+
+# # przyklad
+               
+# tab = [8,4,6,5,9]
+# wstaw(tab)
+# print(tab)
+# ile = 100
+# tab = [random.randint(1,ile) for _ in range(ile)]
+# start_time = time.time()
+# wstaw(tab)
+# end_time = time.time()
+# print("Czas wykonania sortowania to: ", end_time - start_time)
+
+
+# sortowanie dziel i rządź
+tab1 = [1,3,5,7]
+tab2 = [2,4,6,8]
+def sortownia(arr1, arr2):
+    arr = arr1 + arr2
+    print(arr.sort())
+sortownia(tab1, tab2)
